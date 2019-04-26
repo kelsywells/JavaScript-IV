7edf1c91e8e3f29e8c1b1923e585fd8391fbf8e0
Kelsy <kelsywells@gmail.com>
2019-04-25 18:16:02 -0700

    finished creating all classes & methods, just working on a couple small bugs

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,17 +2,49 @@
 
 
 class Person {
-    constructor (attribute) {
-        this.name = name.attribute;
-        this.age = age.attribute;
-        this.location = location.attribute;
-        this.gender = gender.attribute;
+    constructor(attribute) {
+        this.name = attribute.name;
+        this.age = attribute.age;
+        this.location = attribute.location;
+        this.gender = attribute.gender;
     }
 
     speak() {
         return `Hi, my name is ${this.name} and I am from ${this.location}.`
     }
 }
+
+
+class Student extends Person {
+    constructor(attribute){
+        super(attribute);
+        this.previousBackground = attribute.previousBackground;
+        this.className = attribute.className;
+        this.favSubjects = attribute.favSubjects;
+    }
+
+    listSubjects() {
+        return `${this.favSubjects}`
+    }
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${this.subject}.`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${this.subject}.`
+    }
+}
+
+const student1 = new Student ({
+    name: 'jonny',
+    age: 16,
+    location: 'seattle',
+    gender: 'male',
+    previousBackground: 'myspace',
+    favSubjects: ['math', 'css', 'javascript']
+})
+
+
 
 class Instructor extends Person {
     constructor(attribute){
@@ -24,35 +56,59 @@ class Instructor extends Person {
     }
 
     demo(subject) {
-        return `Today we are learning about ${this.subject}.`
+        return `Today we are learning about ${subject}.`
     }
 
     grade(subject) {
-        return `${student.name} receives a perfect score on ${this.subject}.`
+        return `${student1.name} receives a perfect score on ${subject}.`
     }
 }
 
 
-//#### Student
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+const instructor1 = new Instructor ({
+    name: 'teach',
+    age: 23,
+    location: 'cali',
+    gender: 'male',
+    specialty: 'front-end',
+    favLanguage: 'html',
+    catchPhrase: 'yo yo code bros'
+})
+
+console.log(instructor1.demo('javascript'));
+
+console.log(instructor1.grade('html'));
 
 
-class Student extends Person {
-    constructor(attributes){
-        super(attributes);
-        this.previousBackground = attributes.previousBackground;
-        this.className = attributes.className;
-        this.favSubjects = attributes.favSubjects;
+class ProjectManager extends Instructor {
+    constructor(attribute){
+        super(attribute);
+        console.log(attribute);
+        this.gradClassName = attribute.gradClassName;
+        this.favInstructor = attribute.favInstructor;
+   }
+
+   standUp(slackChannel) {
+       return `${projectManager1.name} announces to ${slackChannel}, @channel, standy times!`
+       }
+
+    debugsCode(student, subject) {
+        return `${projectManager1.name} debugs ${student}'s code on ${subject}.`
     }
-
-    
 }
+
+
+
+const projectManager1 = new ProjectManager {
+    name: 'barb',
+    age: 35,
+    location: 'houston',
+    gender: 'female',
+    gradClassName: 'CS1',
+    favInstructor: 'keiran',
+}
+
+console.log(projectManager1.standUp('webPT'));
+
+console.log(projectManager1.debugsCode('franny', 'js'));
+
